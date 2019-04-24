cdbaa5bbe88ad988ea0b22200527a3e90c89bd0e
Max Timkovich <mtimkovich@gmail.com>
2019-04-24 03:56:14 -0700

    Toolbar and login components

---
+++ ui/src/components/Toolbar.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-toolbar app dark color="primary">
+      <router-link to="/" tag="div" class="headline">
+        GAR PR
+      </router-link>
+
+      <v-toolbar-items class="ml-4">
+        <v-btn flat v-for="page of pages"
+                    :key="page.id"
+                    :to="page.route">
+          {{page.name}}
+        </v-btn>
+      </v-toolbar-items>
+
+      <v-spacer></v-spacer>
+
+      <v-menu bottom left offset-y>
+        <template v-slot:activator="{on}">
+          <v-btn icon v-on="on">
+            <v-icon>more_vert</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list dense>
+          <v-list-tile @click.stop="login" >
+            <v-list-tile-title>Login</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    pages: [
+      {id: 1, name: 'Rankings', route: '/'},
+      {id: 2, name: 'Tournaments', route: '/tournaments'},
+      {id: 3, name: 'About', route: '/about'},
+    ],
+  }),
+  methods: {
+    login() {
+      this.$emit('login');
+    }
+  }
+}
+</script>
