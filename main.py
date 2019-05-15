118ec2b829ecf0709afa99b03c249d64c7ae32c9
Max Timkovich <mtimkovich@gmail.com>
2019-05-14 17:24:29 -0700

    cleaned up main

--- main.py
+++ main.py
@@ -1,40 +1,14 @@
-# Copyright 2016 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# [START app]
+import flask
 import logging
 import os
 import urlparse
+
 import challonge
 
-# [START imports]
-import flask
-# [END imports]
-
-# [START create_app]
 app = flask.Flask(__name__)
-# [END create_app]
 
 
-@app.route('/')
-def home():
-    return flask.render_template('index.html')
-
-
-@app.route('/api/upload_tournament', methods=['POST'])
-def upload_tournament():
-    url = flask.request.form['url']
+def url_to_id(url):
     parsed_url = urlparse.urlparse(url)
     split_netloc = parsed_url.netloc.split('.')
     if len(split_netloc) == 3 and split_netloc[0] != 'www':
@@ -48,6 +22,13 @@ def upload_tournament():
     else:
         challonge_id = path
 
+    return challonge_id
+
+@app.route('/api/upload_tournament', methods=['POST'])
+def upload_tournament():
+    url = flask.request.form['url']
+
+    challonge_id = url_to_id(url)
     scraper = challonge.ChallongeScraper(challonge_id)
 
     print scraper.get_players()
@@ -59,35 +40,8 @@ def upload_tournament():
     })
 
 
-# [START form]
-@app.route('/form')
-def form():
-    return flask.render_template('form.html')
-# [END form]
-
-
-# [START submitted]
-@app.route('/submitted', methods=['POST'])
-def submitted_form():
-    name = flask.request.form['name']
-    email = flask.request.form['email']
-    site = flask.request.form['site_url']
-    comments = flask.request.form['comments']
-
-    # [END submitted]
-    # [START render_template]
-    return flask.render_template(
-        'submitted_form.html',
-        name=name,
-        email=email,
-        site=site,
-        comments=comments)
-    # [END render_template]
-
-
 @app.errorhandler(500)
 def server_error(e):
     # Log the error and stacktrace.
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
-# [END app]
